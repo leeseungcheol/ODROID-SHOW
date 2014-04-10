053e287ed293f13065c9be714077c037ce8f622e
john lee <john.lee@hardkernel.com>
2014-04-10 12:06:17 +0900

    add example

---
+++ exmaple/port_open.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
+
+#define baudrate	B500000
+
+int main(void)
+{
+	int usbdev;
+	struct termios options;
+
+	usbdev = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
+
+	if (usbdev == -1)
+		perror("open_port : Unable to open:");
+
+	tcgetattr(usbdev, &options);
+
+	cfsetispeed(&options, baudrate);
+	cfsetospeed(&options, baudrate);
+
+	options.c_cflag |= CS8;
+	options.c_iflag |= IGNBRK;
+	options.c_iflag &= ~( BRKINT | ICRNL | IMAXBEL | IXON);
+	options.c_oflag &= ~( OPOST | ONLCR );
+	options.c_lflag &= ~( ISIG | ICANON | IEXTEN | ECHO | ECHOE | ECHOK | ECHOCTL | ECHOKE);
+	options.c_lflag |= NOFLSH;
+	options.c_cflag &= ~CRTSCTS;
+
+	tcsetattr(usbdev, TCSANOW, &options);
+
+	while(1)
+		sleep(0.2);
+
+	return 0;
+}
