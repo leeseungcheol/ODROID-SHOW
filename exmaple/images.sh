ab51284021957310a465806c029fb8d9184be162
john lee <john.lee@hardkernel.com>
2014-04-10 12:10:21 +0900

    add example

--- exmaple/images.sh
+++ exmaple/images.sh
@@ -12,19 +12,19 @@ do
 		kill $subppid
 		exit
 	fi
-		echo -ne "\e[0r" > /dev/ttyUSB1
+		echo -ne "\e[0r" > /dev/ttyUSB0
 		sleep 0.2
-		echo -ne "\e[0;0,240;320i" > /dev/ttyUSB1
-		cat penguin.raw > /dev/ttyUSB1
+		echo -ne "\e[0;0,240;320i" > /dev/ttyUSB0
+		cat penguin.raw > /dev/ttyUSB0
 		sleep 0.2
-		echo -ne "\e[1r" > /dev/ttyUSB1
+		echo -ne "\e[1r" > /dev/ttyUSB0
 		sleep 0.2
-		echo -ne "\e[0;0,320;240i" > /dev/ttyUSB1
-		cat butterfly.raw > /dev/ttyUSB1
+		echo -ne "\e[0;0,320;240i" > /dev/ttyUSB0
+		cat butterfly.raw > /dev/ttyUSB0
 		sleep 0.2
-		echo -ne "\e[0r" > /dev/ttyUSB1
+		echo -ne "\e[0r" > /dev/ttyUSB0
 		sleep 0.2
-		echo -ne "\e[0;0,240;320i" > /dev/ttyUSB1
-		cat woof.raw > /dev/ttyUSB1
+		echo -ne "\e[0;0,240;320i" > /dev/ttyUSB0
+		cat woof.raw > /dev/ttyUSB0
 		sleep 0.2
 done
