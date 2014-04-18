2ebf75f7ccffe790a572852b262de147de10d19c
John lee <john.lee@hardkernel.com>
2014-04-17 23:54:41 -0700

    update cpu_status.sh

--- exmaple/cpu_status.sh
+++ exmaple/cpu_status.sh
@@ -7,15 +7,12 @@ trap "flag=1" SIGINT SIGKILL SIGTERM
 ./port_open &
 subppid=$!
 
-serialPort="/dev/ttyUSB0"
-
 function cpu_state {
 	cpuFreqM=$(echo "scale=0; " `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq` "/1000" | bc)
 	cpuTempM=$(echo "scale=1; " `cat /sys/class/thermal/thermal_zone0/temp` "/1000" | bc)
 }
 
-sleep 0.1
-echo -ne "\ec\e[2s\e[3r" > $serialPort
+echo -ne "\ec\e[2s\e[3r" > /dev/ttyUSB0
 sleep 0.1
 
 while true
@@ -24,45 +21,29 @@ do
 		kill $subppid
 		exit
 	fi
-		echo -ne "\e[H\e[35mTime : \e[36m" > $serialPort
-		date +"%T" > $serialPort
+		echo -ne "\e[H\e[35mTime : \e[36m" > /dev/ttyUSB0
+		date +"%T" > /dev/ttyUSB0
 		sleep 0.1
-		echo -ne "\eE\eM\e[32m" > $serialPort
+		echo -ne "\eE\eM\e[32mcore0 : \e[31m" > /dev/ttyUSB0
 		sleep 0.1
-		mpstat -P 0 | grep -A1 "usr" | grep -v "usr" | awk '{print "core"$3" : "}' > $serialPort
+		mpstat -P 0 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > /dev/ttyUSB0
 		sleep 0.1
-		echo -ne "\e[31m\eM" > $serialPort
+		echo -ne "\eE\eM\e[32mcore1 : \e[31m" > /dev/ttyUSB0
 		sleep 0.1
-		mpstat -P 0 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > $serialPort
+		mpstat -P 1 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > /dev/ttyUSB0
 		sleep 0.1
-		echo -ne "\eE\eM\e[32m" > $serialPort
+		echo -ne "\eE\eM\e[32mcore2 : \e[31m" > /dev/ttyUSB0
 		sleep 0.1
-		mpstat -P 1 | grep -A1 "usr" | grep -v "usr" | awk '{print "core"$3" : "}' > $serialPort
+		mpstat -P 2 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > /dev/ttyUSB0
 		sleep 0.1
-		echo -ne "\e[31m\eM" > $serialPort
+		echo -ne "\eE\eM\e[32mcore3 : \e[31m" > /dev/ttyUSB0
 		sleep 0.1
-		mpstat -P 1 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > $serialPort
-		sleep 0.1
-		echo -ne "\eE\eM\e[32m" > $serialPort
-		sleep 0.1
-		mpstat -P 2 | grep -A1 "usr" | grep -v "usr" | awk '{print "core"$3" : "}' > $serialPort
-		sleep 0.1
-		echo -ne "\e[31m\eM" > $serialPort
-		sleep 0.1
-		mpstat -P 2 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > $serialPort
-		sleep 0.1
-		echo -ne "\eE\eM\e[32m" > $serialPort
-		sleep 0.1
-		mpstat -P 3 | grep -A1 "usr" | grep -v "usr" | awk '{print "core"$3" : "}' > $serialPort
-		sleep 0.1
-		echo -ne "\e[31m\eM" > $serialPort
-		sleep 0.1
-		mpstat -P 3 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > $serialPort
+		mpstat -P 3 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > /dev/ttyUSB0
 		sleep 0.1
 		cpu_state
-		echo -ne "\eE\eM" > $serialPort
+		echo -ne "\eE\eM" > /dev/ttyUSB0
 		sleep 0.1
-		echo -ne "\e[33mCPU Freq:   \e[37m"$cpuFreqM"MHz   \eE" > $serialPort
-		echo -ne "\e[33mCPU Temp:   \e[37m$cpuTempM\e   " > $serialPort
+		echo -ne "\e[33mCPU Freq: \e[37m"$cpuFreqM"MHz   \eE" > /dev/ttyUSB0
+		echo -ne "\e[33mCPU Temp: \e[37m$cpuTempM\e   " > /dev/ttyUSB0
 		sleep 1
 done
