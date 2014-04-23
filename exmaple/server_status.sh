d5f883dd6fdb9e9560d3759b8d7eab717ebc03ec
john lee <john.lee@hardkernel.com>
2014-04-23 16:38:50 +0900

    update example

--- exmaple/server_status.sh
+++ exmaple/server_status.sh
@@ -9,7 +9,7 @@ trap "flag=1" SIGINT SIGKILL SIGTERM
 subppid=$!
 
 sleep 0.1
-echo -ne "\ec\e[1s\e[3r" > $serialPort
+echo -ne "\ec\e[2s\e[3r" > $serialPort
 sleep 0.1
 
 function cpu_state {
@@ -28,36 +28,40 @@ do
 		echo -ne "\e[H\e[35mTime : \e[36m" > $serialPort
 		date +"%T" > $serialPort
 		sleep 0.1
-		echo -ne "\eE\eM\e[32mcore0:\e[31m" > $serialPort
+		echo -ne "\r\e[32mcore0:\e[31m" > $serialPort
 		sleep 0.1
 		mpstat -P 0 | grep -A1 "usr" | grep -v "usr" | awk '{print""$4"% "}' > $serialPort
 		sleep 0.1
 		echo -ne "\eM\e[32mcore1:\e[31m" > $serialPort
 		sleep 0.1
-		mpstat -P 1 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"%   "}' > $serialPort
+		mpstat -P 1 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"% "}' > $serialPort
 		sleep 0.1
-		echo -ne "\eM\e[32mcore2:\e[31m" > $serialPort
+		echo -ne "\r\e[32mcore2:\e[31m" > $serialPort
 		sleep 0.1
 		mpstat -P 2 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"% "}' > $serialPort
 		sleep 0.1
-		echo -ne "\r\e[33m" > $serialPort
+		echo -ne "\eM\e[32mcore3:\e[31m" > $serialPort
+		sleep 0.1
+		mpstat -P 3 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"% "}' > $serialPort
+		sleep 0.1
+		echo -ne "\r\e[32mcore4:\e[31m" > $serialPort
+		sleep 0.1
+		mpstat -P 4 | grep -A1 "usr" | grep -v "usr" | awk '{print""$4"% "}' > $serialPort
+		sleep 0.1
+		echo -ne "\eM\e[32mcore5:\e[31m" > $serialPort
+		sleep 0.1
+		mpstat -P 5 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"% "}' > $serialPort
+		sleep 0.1
+		echo -ne "\r\e[32mcore6:\e[31m" > $serialPort
+		sleep 0.1
+		mpstat -P 6 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"% "}' > $serialPort
+		sleep 0.1
+		echo -ne "\eM\e[32mcore7:\e[31m" > $serialPort
+		sleep 0.1
+		mpstat -P 7 | grep -A1 "usr" | grep -v "usr" | awk '{print ""$4"% "}' > $serialPort
+		echo -ne "\r\e[35m" > $serialPort
 		sleep 0.1
 		df -h | grep "sda" | awk '{print ""$1"\n\rSize  : "$2"\n\rUsed  : "$3"\n\rAvail : "$4"\n\rUse   : "$5""}' > $serialPort
-		echo -ne "$sdaInfo" > $serialPort
-		sleep 0.1
-		echo -ne "\r\e[33m" > $serialPort
-		sleep 0.1
-		df -h | grep "sdb" | awk '{print ""$1"\n\rSize  : "$2"\n\rUsed  : "$3"\n\rAvail : "$4"\n\rUse   : "$5""}' > $serialPort
-		echo -ne "$sdaInfo" > $serialPort
-		sleep 0.1
-		echo -ne "\r\e[33m" > $serialPort
-		sleep 0.1
-		df -h | grep "sdc" | awk '{print ""$1"\n\rSize  : "$2"\n\rUsed  : "$3"\n\rAvail : "$4"\n\rUse   : "$5""}' > $serialPort
-		echo -ne "$sdaInfo" > $serialPort
-		sleep 0.1
-		echo -ne "\r\e[33m" > $serialPort
-		sleep 0.1
-		df -h | grep "sdd" | awk '{print ""$1"\n\rSize  : "$2"\n\rUsed  : "$3"\n\rAvail : "$4"\n\rUse   : "$5""}' > $serialPort
 		echo -ne "$sdaInfo" > $serialPort
 		sleep 0.1
 		cpu_state
